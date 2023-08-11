5b6a2126024797da1968db92d74092593104d153
Syipmong <Syipmong5@gmail.com>
2023-08-11 16:26:09 -0700

    new changes

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    """printing the addition of 1 and 2"""
 
-def add(a, b):
-    """My addition function
-    Args:
-        a: first integer
-        b: second integer
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
