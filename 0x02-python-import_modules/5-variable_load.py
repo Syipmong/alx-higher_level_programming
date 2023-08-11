6c9774724c4224fb465c0ca9793cb827e2df4038
Syipmong <Syipmong5@gmail.com>
2023-08-11 16:39:01 -0700

    m

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """ brings the variable a from the file and prints its value."""
+    import variable_load_5
+    print("{}".format(variable_load_5.a))
